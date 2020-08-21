9c53713db6876c4bf2917ef531f20a4e8710760a
Tyson Lackey <lackeyt90@gmail.com>
2020-08-21 16:07:23 -0700

    add seed data

---
+++ AnimalShelterApi/Migrations/20200821230635_modelbuilder.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AnimalShelterApi.Migrations
+{
+    public partial class modelbuilder : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Cats",
+                columns: new[] { "CatId", "Color", "Description", "Name", "Temperament" },
+                values: new object[,]
+                {
+                    { 1, "Black", "testDesc", "Cat1", "Calm" },
+                    { 2, "Tan", "testDesc", "Cat2", "Anxious" },
+                    { 3, "White", "testDesc", "Cat3", "Hyper" },
+                    { 4, "Cream", "testDesc", "Cat4", "Energetic" },
+                    { 5, "Black", "testDesc", "Cat5", "lazy" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Dogs",
+                columns: new[] { "DogId", "Color", "Description", "Name", "Temperament" },
+                values: new object[,]
+                {
+                    { 1, "Black", "testDesc", "Dog1", "Calm" },
+                    { 2, "Tan", "testDesc", "Dog2", "Anxious" },
+                    { 3, "White", "testDesc", "Dog3", "Hyper" },
+                    { 4, "Cream", "testDesc", "Dog4", "Energetic" },
+                    { 5, "Black", "testDesc", "Dog5", "lazy" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Cats",
+                keyColumn: "CatId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Cats",
+                keyColumn: "CatId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Cats",
+                keyColumn: "CatId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Cats",
+                keyColumn: "CatId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Cats",
+                keyColumn: "CatId",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Dogs",
+                keyColumn: "DogId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Dogs",
+                keyColumn: "DogId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Dogs",
+                keyColumn: "DogId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Dogs",
+                keyColumn: "DogId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Dogs",
+                keyColumn: "DogId",
+                keyValue: 5);
+        }
+    }
+}
--- AnimalShelterApi/Models/AnimalShelterApiContext.cs
+++ AnimalShelterApi/Models/AnimalShelterApiContext.cs
@@ -11,5 +11,26 @@ namespace AnimalShelterApi.Models
     }
     public DbSet<Dog> Dogs {get; set;}
     public DbSet<Cat> Cats {get; set;}
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+      builder.Entity<Dog>()
+        .HasData(
+          new Dog {DogId=1, Name="Dog1", Color="Black", Temperament="Calm", Description="testDesc"},
+          new Dog {DogId=2, Name="Dog2", Color="Tan", Temperament="Anxious", Description="testDesc"},
+          new Dog {DogId=3, Name="Dog3", Color="White", Temperament="Hyper", Description="testDesc"},
+          new Dog {DogId=4, Name="Dog4", Color="Cream", Temperament="Energetic", Description="testDesc"},
+          new Dog {DogId=5, Name="Dog5", Color="Black", Temperament="lazy", Description="testDesc"}
+        );
+      
+      builder.Entity<Cat>()
+        .HasData(
+          new Cat {CatId=1, Name="Cat1", Color="Black", Temperament="Calm", Description="testDesc"},
+          new Cat {CatId=2, Name="Cat2", Color="Tan", Temperament="Anxious", Description="testDesc"},
+          new Cat {CatId=3, Name="Cat3", Color="White", Temperament="Hyper", Description="testDesc"},
+          new Cat {CatId=4, Name="Cat4", Color="Cream", Temperament="Energetic", Description="testDesc"},
+          new Cat {CatId=5, Name="Cat5", Color="Black", Temperament="lazy", Description="testDesc"}
+        );
+    }
+    
   }
 }
